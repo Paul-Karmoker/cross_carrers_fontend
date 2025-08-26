a2c2ebb5931f64319f8d8f38128d4df87e7bb148
ibrahim23dev <ibrahim.soft.dev23@gmail.com>
2025-08-26 22:04:04 +0600

    upload

--- src/ResumeMaker/resumeForm.jsx
+++ src/ResumeMaker/resumeForm.jsx
@@ -3,8 +3,8 @@
 import { useEffect, useState, useCallback, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import Navbar from "../Components/Navbar";
-import Footer from "../Components/Footer";
+import Navbar from "../Components/navbar";
+import Footer from "../Components/footer";
 import {
   useCreateResumeMutation,
   useGetResumeQuery,
