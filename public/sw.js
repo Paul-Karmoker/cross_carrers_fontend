634ddac6c3a05f38a51d9e2d1ff10f3b6bec532c
Paul-Connect <paulkarmoker1985@gmail.com>
2026-03-28 21:55:07 +0600

    monetag

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10760476
+    "zoneId": 10798719
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
